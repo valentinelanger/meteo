b403a74ee4fe8b9ae3021c7052f0e8379b92e400
Valentine Langer <valentine.langer@gmail.com>
2017-07-03 12:31:17 +0200

    understood API response - for now it prints temp for every record - evolution time / I dont understand why there are various times the same hour exactly depending on city etc

---
+++ app/controllers/meteos_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'json'
+
+class MeteosController < ApplicationController
+  API_KEY = 'fbd2908be34da7b2'
+
+  def ask
+  end
+
+  def answer
+    if params[:city].present? && params[:date].present?
+      @city = params[:city]
+      @date = params[:date]
+      @meteos = weather_answer(params[:city], params[:date])
+    end
+  end
+
+  private
+
+  def weather_answer(city, date)
+    uri = "http://api.wunderground.com/api"
+    filepath = "#{uri}/#{API_KEY}/history_#{date}/q/FR/#{city}.json"
+    meteo_attempt = open(filepath).read
+    meteo = JSON.parse(meteo_attempt)
+    return meteo["history"]["observations"]
+  end
+end
