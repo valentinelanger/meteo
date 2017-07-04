40285ed3575585b9e7581e7eb9783f58bce6267e
Valentine Langer <valentine.langer@gmail.com>
2017-07-04 23:25:28 +0200

    dynamic results when submitting the form with angularjs service

--- app/assets/javascripts/services/forecast.js
+++ app/assets/javascripts/services/forecast.js
@@ -1,14 +1,26 @@
 app.factory('forecast', ['$http', function($http) {
-  var city = document.getElementById("ville").value;
-  var date = document.getElementById("datepicker").value;
+  var city = getParameterByName('city');
+  var date = getParameterByName('date');
 
   return $http({
     method: 'GET',
-    url: '/answer.json?city=Paris&date=25%2F06%2F2017',
+    url: '/answer.json?city='+city+'&date='+date,
     headers: { 'Accept': 'application/json, */*'},
   }).then(function successCallback(response) {
-    return response.data[0];
+    console.log(response.data);
+    return response.data;
   }, function errorCallback(response) {
-    return response.data[0];
+    return response.data;
   });
+
+
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 }]);
